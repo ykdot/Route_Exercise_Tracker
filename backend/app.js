74db871e38f92bc4edce3a0d43e609372ec4ea58
ykdo <kdot147x@gmail.com>
2024-02-17 23:31:23 -0800

    add: basic setup

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const usersRoutes = require('./routes/users-routes');
+
+
+
+const app = express();
+
+
+
+app.listen(5000);
