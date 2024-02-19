700aa87912bd07d6ca5ec23fc290c9dc00fbe2ba
ykdo <kdot147x@gmail.com>
2024-02-19 01:33:56 -0800

    add: desktop to mobile transition for the nav bar

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 function Navbar() {
-  
+  const [ mobileList, setMobileList ] = useState(false);
+
+  let mobileShow;
+  // show list or not for mobile version
+  if (mobileList) {
+    mobileShow = 'show-list';
+  }
+
   return (
     <nav className={classes['main-nav']}>
-      <h1>Route Tracker</h1>
+      <h1 className={classes.title}>Route Tracker</h1>
 
-      <nav className={classes['link-nav']}>
+      <button className={classes['link-icon']} onClick={() => setMobileList(!mobileList)}>
+        <img className={classes.icon} src="/hamburger-icon.svg" alt="Hamburger Icon" />
+      </button>
+      <nav className={`${classes['link-nav']} ${classes[mobileShow]}`}>
         <Link className={classes['link-button']}>Home</Link>
         <Link className={classes['link-button']}>Routes</Link>
+        <Link className={classes['link-button']}>Log In</Link>
+
       </nav>
     </nav>
   );
