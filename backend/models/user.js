7767bc60b88fc2abfd1e14c48c1f0af3481f3481
second one <icekap49@gmail.com>
2024-03-19 22:36:24 -0700

    add: polarID in user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,6 +8,7 @@ const userSchema = new Schema({
   username: { type: String, required: true, minlength: 3, maxLength: 20},
   password: { type: String, required: true, minlength: 6}, 
   routes: { type: Map, of: Array, default: () => ({}), ref: 'Route'},
+  polarID: { type: Number, default: 0},
 });
 
 // make sure we have a unique email, query email as fast as possible, mke sure user can be created only with unique names
